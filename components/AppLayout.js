e11d82c12abddc2efa22c49f98d142b79620a2ec
Vicent Pérez <vicennt@gmail.com>
2020-11-24 16:27:50 +0100

    extracting styles from components

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,8 +1,6 @@
 import Head from 'next/head';
-import { breakpoints, fonts, colors } from '../styles/theme';
-import { addOpacityToColor } from '../styles/utils';
+import styles, {globalStyles} from './styles';
 
-const backgroundColor = addOpacityToColor(colors.primary, 0.3);
 
 
 export default function AppLayout({ children }) {
@@ -17,56 +15,8 @@ export default function AppLayout({ children }) {
           {children}
         </main>
       </div>
-
-      <style jsx>{`
-          div {
-            display: grid;
-            height: 100vh;
-            place-items: center;
-          }
-
-          main {
-            background: #fff;
-            border-radius: 10px;
-            box-shadow: 0 10px 25px rgba(0, 0, 0, .1);
-            height: 100%;
-            width: 100%;
-          }
-
-          @media (min-width: ${breakpoints.mobile}){
-            main {
-              height: 90vh;
-              width: ${breakpoints.mobile};
-            }
-          }
-
-        `}
-      </style>
-      
-      <style jsx global>
-        {`
-          html,
-          body {
-            background-image: 
-              radial-gradient(${backgroundColor} 1px, transparent 1px),
-              radial-gradient(${backgroundColor} 1px, transparent 1px);
-            background-position: 0 0, 25px 25px;
-            background-size: 50px 50px;
-            padding: 0;
-            margin: 0;
-            font-family: ${fonts.base};
-          }
-
-          a {
-            color: inherit;
-            text-decoration: none;
-          }
-
-          * {
-            box-sizing: border-box;
-          }
-        `}
-      </style>
+      <style jsx>{styles}</style>
+      <style jsx global>{globalStyles}</style>
     </>
   );
 }
