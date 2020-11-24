89535801ce732651e5a2157077784c8623cf55f2
Vicent Pérez <vicennt@gmail.com>
2020-11-24 16:51:01 +0100

    logo uploaded & main page with basic style

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,45 @@
 import Link from 'next/link';
 import AppLayout from '../components/AppLayout';
+import { breakpoints, fonts, colors } from "../styles/theme";
 
 
 export default function Home() {
   return (
     <div>
       <AppLayout>
-        <h1><Link href="/"><a>devter</a></Link></h1>
+        <section>
+          <img src="/logo.png" alt="Logo"/>
+          <h1>Devter</h1>
+          <h2>Talk about development with developers</h2>   
+        </section>
       </AppLayout>
+      
+      <style jsx>{`
+
+        section {
+          display: grid;
+          height: 100%;
+          place-items: center;
+          place-content: center;
+        }
+
+        img{
+          width: 120px;
+        }
+
+        h1 {
+          color: ${colors.primary};
+          font-weight: 800;
+          margin-bottom: 8px;
+        }
+
+        h2 {
+          font-size: 16px;
+          color: ${colors.secondary};
+          margin: 0;
+        }
+        
+      `}</style>
     </div>
   )
 }
