ef8e56b88bd27276f9540b4f6f8629fdc22d650e
Vicent Pérez <vicennt@gmail.com>
2020-11-27 13:58:20 +0100

    navigation bar created & slyled

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,8 +1,13 @@
 import AppLayout from "components/AppLayout";
 import { useState, useEffect } from "react";
+import Link from "next/link";
 import Devit from "components/Devit";
 import useUser from "hooks/useUser";
 import { fetchLatestDevits } from "firebase/client";
+import Create from "components/Icons/Create";
+import Home from "components/Icons/Home";
+import Search from "components/Icons/Search";
+import { colors } from "styles/theme";
 
 export default function HomePage() {
   const [timeline, setTimeline] = useState([]);
@@ -38,10 +43,30 @@ export default function HomePage() {
             }
           )}
         </section>
-        <nav></nav>
+        <nav>
+          <Link href="/home">
+            <a>
+              <Home width={32} height={32} stroke="#09f" />
+            </a>
+          </Link>
+          <Link href="/search">
+            <a>
+              <Search width={32} height={32} stroke="#09f" />
+            </a>
+          </Link>
+          <Link href="/compose/tweet">
+            <a>
+              <Create width={32} height={32} stroke="#09f" />
+            </a>
+          </Link>
+        </nav>
       </AppLayout>
 
       <style jsx>{`
+        section {
+          flex: 1;
+        }
+
         header {
           background: #ffffffaa;
           backdrop-filter: blur(5px);
@@ -63,10 +88,29 @@ export default function HomePage() {
         nav {
           background: #fff;
           bottom: 0;
+          display: flex;
           border-top: 1px solid #eee;
           height: 49px;
           position: sticky;
           width: 100%;
+        }
+
+        nav a {
+          align-items: center;
+          display: flex;
+          flex: 1 1 auto;
+          height: 100%;
+          justify-content: center;
+        }
+
+        nav a:hover {
+          background: radial-gradient(#0099ff22 15%, transparent 16%);
+          background-size: 180px 180px;
+          background-position: center;
+        }
+
+        nav a:hover > :global(svg) {
+          stroke: ${colors.primary};
         }
       `}</style>
     </>
